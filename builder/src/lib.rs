56ab58d8de87640341d44bb1b4e8885ac8b92337
Robert Markl <robert.markl@tum.de>
2020-06-13 19:56:55 +0200

    builder: implement setter functions

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,33 +2,21 @@ extern crate proc_macro;
 extern crate quote;
 extern crate syn;
 
-// use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let derive_input: syn::DeriveInput = syn::parse_macro_input!(input);
 
-    let name = derive_input.ident;
+    let name = &derive_input.ident;
     let builder_name = format_ident!("{}Builder", name);
-
-    let fields = {
-        if let syn::Data::Struct(data_struct) = derive_input.data {
-            if let syn::Fields::Named(fields) = data_struct.fields {
-                fields.named
-            } else {
-                unimplemented!()
-            }
-        } else {
-            unimplemented!()
-        }
-    };
+    let fields = get_fields(&derive_input);
 
     let struct_body = fields
         .iter()
         .map(|x| {
-            let name = x.ident.as_ref().expect("Expected field identifier");
-            let field_type = &x.ty;
+            let name = x.name;
+            let field_type = x.field_type;
             quote! {
                 #name: Option<#field_type>
             }
@@ -38,12 +26,14 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let builder_body = fields
         .iter()
         .map(|x| {
-            let name = x.ident.as_ref().expect("Expected field identifier");
+            let name = x.name;
             quote! {
                 #name: None
             }
         })
         .collect::<Vec<proc_macro2::TokenStream>>();
+
+    let setter_functions = derive_setter_functions(&fields);
 
     let out = quote! {
         #[derive(Debug, PartialEq)]
@@ -59,7 +49,58 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             }
         }
 
+        impl #builder_name {
+            #setter_functions
+        }
+
     };
 
     out.into()
+}
+
+struct Field<'f> {
+    name: &'f syn::Ident,
+    field_type: &'f syn::Type,
+}
+
+fn get_fields<'f>(derive_input: &'f syn::DeriveInput) -> Vec<Field<'f>> {
+    let fields = {
+        if let syn::Data::Struct(data_struct) = &derive_input.data {
+            if let syn::Fields::Named(fields) = &data_struct.fields {
+                &fields.named
+            } else {
+                unimplemented!()
+            }
+        } else {
+            unimplemented!()
+        }
+    };
+
+    fields
+        .iter()
+        .map(|x| Field {
+            name: x.ident.as_ref().expect("Expected identifier"),
+            field_type: &x.ty,
+        })
+        .collect()
+}
+
+fn derive_setter_functions(fields: &[Field]) -> proc_macro2::TokenStream {
+    let setter_functions = fields
+        .iter()
+        .map(|field| {
+            let name = field.name;
+            let field_type = field.field_type;
+            quote! {
+                fn #name(&mut self, x: #field_type) -> &mut Self{
+                    self.#name = Some(x);
+                    self
+                }
+            }
+        })
+        .collect::<Vec<proc_macro2::TokenStream>>();
+
+    quote! {
+        #(#setter_functions)*
+    }
 }
